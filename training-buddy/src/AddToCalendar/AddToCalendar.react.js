08c969026f97c4652b0b2f3e9fc2f1f9a0327dfb
Srivatsan <vatsank@gmail.com>
2019-05-14 18:21:08 +0530

    Added Form

--- training-buddy/src/AddToCalendar/AddToCalendar.react.js
+++ training-buddy/src/AddToCalendar/AddToCalendar.react.js
@@ -17,33 +17,49 @@ class AddToCalendar extends React.Component {
         }
     }
 
-    handleChange = () =>{
+    handleChange = (event) =>{
+     console.log(event);
+        let value = event.target.value;
+        let name = event.target.name;
 
-    }
-
-    handleSubmit = () =>{
+        this.setState( prevState => ({ newCalendar : 
+            {...prevState.newCalendar, [name]: value  }
+        }
+        ));
         
+    }
+    url = "http://localhost:4000/calendar";
+    handleSubmit = (event) =>{
+
+        event.preventDefault();
+
+        fetch(this.url,{
+            method:'POST',
+            body:JSON.stringify(this.state.newCalendar),
+            headers:{'Content-Type':'application/json'}
+        });
     }
 
     render() {
         return (
-            <form className='form'>
+            <form className='form' onSubmit={this.handleSubmit}>
       
             <div className='form-group'>
                 <label htmlFor="">Id</label>
-                <input type="text" className="form-control"/>
+                <input type="text" name='id' className="form-control" 
+                   value={this.state.newCalendar.id} onChange={this.handleChange}/>
             </div>
             <div className='form-group' >
                 <label htmlFor="">Course Name</label>
-                <input type="text" className="form-control"/>
+                <input type="text" name='courseName' className="form-control" value={this.state.newCalendar.courseName} onChange={this.handleChange}/>
             </div>
             <div className='form-group'>
                 <label htmlFor="">Duration</label>
-                <input type="text" className="form-control"/>
+                <input type="text" name='duration' className="form-control" value={this.state.newCalendar.duration} onChange={this.handleChange}/>
             </div>
             <div className='form-group'>
                   <label htmlFor="">Start Date</label>
-                  <input type="text" className="form-control"/>
+                  <input type="text"  name='startDate' className="form-control" value={this.state.newCalendar.startDate} onChange={this.handleChange}/>
             </div>
             <div>
               <input type="submit" value="Add" className='btn btn-info'/></div>
