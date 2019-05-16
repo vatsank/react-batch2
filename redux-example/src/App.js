a0e8dce42addd17d03110dd8f9a519285a11b9ae
Srivatsan <vatsank@gmail.com>
2019-05-16 14:43:30 +0530

    Routing Added

--- redux-example/src/App.js
+++ redux-example/src/App.js
@@ -5,20 +5,43 @@ import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import Greeter from './Greeter/Greeter.react';
 import ShowTrainers from './ShowTrainers/ShowTrainers.react';
 import AddTrainer from './AddTrainer/AddTrainer.react';
+import  {BrowserRouter,Route,Link,Switch} from 'react-router-dom';
 import axios from 'axios'
+
 const postToServer = (values)=>{
  console.log(values);
 
  axios.post('http://localhost:4000/trainers',values).then(res =>console.log('one trainer Added'));
 }
+
 function App() {
   return (
     <div className="App">
    <h1>React- Redux Example</h1>
    
-  <Greeter></Greeter> 
-   <ShowTrainers></ShowTrainers>
-   <AddTrainer onSubmit={postToServer}></AddTrainer>
+   <BrowserRouter>
+   <ul>
+   <li> <Link to='/home'>Greeting Example</Link>
+   </li>
+   <li> <Link to='/add'>Add Trainer</Link>
+   </li>
+   <li>
+   <Link to='/show'>Show Trainers</Link>
+   
+   </li>
+   
+    </ul>
+    <Switch>
+    <Route path='/home' component={Greeter}/>
+    <Route path='/show' component={ShowTrainers}/>
+    <Route path='/add'
+      render={ () =><AddTrainer onSubmit={postToServer}></AddTrainer>}/>
+
+    </Switch>
+   </BrowserRouter>
+
+   { /* <AddTrainer onSubmit={postToServer}></AddTrainer>*/}
+
     </div>
   );
 }
--- redux-example/src/index.js
+++ redux-example/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {Provider } from 'react-redux'
-import { ConnectedRouter } from 'react-router-redux'
 import * as serviceWorker from './serviceWorker';
 import store from './store';
 
